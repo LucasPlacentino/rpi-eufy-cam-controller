13109f93a0be982e20e2882bd9a2def566597e53
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2023-07-14 14:00:01 +0200

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,15 @@
+# MIT License
+# by Lucas Placentino
+# Eufy Cam Controller - RaspberryPi
+
+class Controller():
+  cameras = []
+  
+  def __init__(self):
+    self.cameras.add(cam1)
+
+def start():
+  print("Starting...")
+
+if __name__ == "__main__":
+  start()
